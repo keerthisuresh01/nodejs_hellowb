edf21aae70e092801d23b86873242f96e281127a
Keerthi <80613202+keerthisuresh01@users.noreply.github.com>
2021-10-10 20:21:38 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var http=require('http');
-const express=require('express');
-var app=express();
 const PORT=process.env.PORT ||4000;
-app.get('/',(req,res)=>{
+http.createServer(function(req,res){
     res.write("<div><p>keerthis.19it@kongu.edu</p></div>");
     res.write("<h1>Hello World</h1>");
     res.end();
